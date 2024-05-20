6df1b7216c004faa272ac0b63b3b83ff700043d0
razan0r <razanhamad815@gmail.com>
2024-05-20 13:36:55 +0300

    add create,edit

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
 ﻿using BookStore.Data;
+using BookStore.Models;
+using BookStore.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
@@ -16,6 +18,48 @@ namespace BookStore.Controllers
         {
             var categories=context.Categories.ToList();
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryVM categoryVM )
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Create", categoryVM);
+            }
+            var category = new Category()
+            {
+                Name = categoryVM.Name
+            };
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View("Create");
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(CategoryVM categoryVM)
+        {
+           var category = context.Categories.Find(categoryVM.Id);
+            if(category is null)
+            {
+                return NotFound();
+            }
+            category.Name = categoryVM.Name;
+            context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
---
+++ ViewModel/CategoryVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.ViewModel
+{
+    public class CategoryVM
+    {
+
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage ="plz enter name")]
+        [MaxLength(30,ErrorMessage ="max 30")]
+
+        public string Name { get; set; } = null!;
+
+    }
+}
